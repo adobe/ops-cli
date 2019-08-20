0bc4727025e38412728bb4b135d4eb975b7782e8
Anthony Elizondo <anthony.elizondo@gmail.com>
2019-08-20 14:32:16 -0400

    Update sync.py

--- src/ops/cli/sync.py
+++ src/ops/cli/sync.py
@@ -8,6 +8,7 @@
 #OF ANY KIND, either express or implied. See the License for the specific language
 #governing permissions and limitations under the License.
 
+import getpass
 import subprocess
 
 from parser import SubParserConfig
