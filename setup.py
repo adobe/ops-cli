cb7039b76408ac8a85d1355f8b612b854de4c6af
Costi Muraru <costimuraru@gmail.com>
2019-08-22 17:00:33 +0300

    Publish ops-cli to PyPi (#60)
    Signed-off-by: costimuraru <costimuraru@gmail.com>

--- setup.py
+++ setup.py
@@ -8,19 +8,45 @@
 #OF ANY KIND, either express or implied. See the License for the specific language
 #governing permissions and limitations under the License.
 
-from setuptools import setup, find_packages
 import os
 import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+with open('README.md') as f:
+    _readme = f.read()
 
 _mydir = os.path.abspath(os.path.dirname(sys.argv[0]))
 _requires = [ r for r in open(os.path.sep.join((_mydir,'requirements.txt')), "r").read().split('\n') if len(r)>1 ]
 setup(
-    name='ops',
+    name='ops-cli',
     version='1.9',
-    description='Ops simple wrapper',
+    description='Ops - wrapper for Terraform, Ansible, and SSH for cloud automation',
+    long_description=_readme + '\n\n',
+    long_description_content_type='text/markdown',
+    url='https://github.com/adobe/ops-cli',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     author='Adobe',
     author_email='noreply@adobe.com',
-    url='https://github.com/adobe/ops-cli',
+    license='Apache2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ],
     package_dir={'': 'src'},
     packages=find_packages('src'),
     package_data={
