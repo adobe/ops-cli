ab8ae65e65c88efd662bce0612f285bf83775cf3
wenzdey <56051809+wenzdey@users.noreply.github.com>
2019-10-03 08:32:39 -0500

    Moved python env shebang to first line (#70)
    Shebang only works if first line of script.

--- src/ops/inventory/azurerm.py
+++ src/ops/inventory/azurerm.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #Copyright 2019 Adobe. All rights reserved.
 #This file is licensed to you under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License. You may obtain a copy
@@ -7,8 +9,6 @@
 #the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
 #OF ANY KIND, either express or implied. See the License for the specific language
 #governing permissions and limitations under the License.
-
-#!/usr/bin/env python
 #
 # Copyright (c) 2016 Matt Davis, <mdavis@ansible.com>
 #                    Chris Houseknecht, <house@redhat.com>
