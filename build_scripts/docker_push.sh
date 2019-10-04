5104bf08d40ea25d96e2f53987a795cf6255271c
costimuraru <costimuraru@gmail.com>
2019-10-04 19:55:50 +0300

    [RELEASE] - Release version 1.11.5

--- build_scripts/docker_push.sh
+++ build_scripts/docker_push.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag ops adobe/ops-cli:1.11.4
-docker push adobe/ops-cli:1.11.4
+docker tag ops adobe/ops-cli:1.11.5
+docker push adobe/ops-cli:1.11.5
