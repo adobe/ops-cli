1efcee144c1568e358cc95175438d2c68a207dae
Adi Muraru <amuraru@adobe.com>
2019-11-12 13:52:03 +0200

    [RELEASE] - Release version 2.0.3

--- build_scripts/docker_push.sh
+++ build_scripts/docker_push.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag ops adobe/ops-cli:2.0.2
-docker push adobe/ops-cli:2.0.2
+docker tag ops adobe/ops-cli:2.0.3
+docker push adobe/ops-cli:2.0.3
