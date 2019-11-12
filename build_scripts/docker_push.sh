035bf94c01344a80756785087ffd2cbbeec63257
Adi Muraru <amuraru@adobe.com>
2019-11-12 10:37:09 +0200

    [RELEASE] - Release version 1.12.1
    - Fixed pypi release step in travis

--- build_scripts/docker_push.sh
+++ build_scripts/docker_push.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag ops adobe/ops-cli:1.11.12
-docker push adobe/ops-cli:1.11.12
+docker tag ops adobe/ops-cli:1.12.1
+docker push adobe/ops-cli:1.12.1
