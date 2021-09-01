db005f39c903e2ad0c590a634235fb914c5e5b9f
Adi Muraru <amuraru@adobe.com>
2021-09-01 13:25:59 +0300

    Fix docker build

--- build_scripts/build_package.sh
+++ build_scripts/build_package.sh
@@ -4,5 +4,6 @@ set -e
 echo "Building package"
 rm -rf dist/
 export BOTO_CONFIG=/dev/null
+export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 python setup.py sdist bdist_wheel
 ls -l dist/
--- build_scripts/run_tests.sh
+++ build_scripts/run_tests.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Running tests"
 export BOTO_CONFIG=/dev/null
+export CRYPTOGRAPHY_DONT_BUILD_RUST=1
+
 pip install --no-cache-dir -r requirements.txt
 pip install pytest
 
