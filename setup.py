ca7db4de5176f403c2c265b18ccc4f6bb1561229
Adi Muraru <amuraru@adobe.com>
2021-09-13 17:01:16 +0300

    [RELEASE] - Release version 2.1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ _mydir = os.path.abspath(os.path.dirname(sys.argv[0]))
 _requires = [r for r in open(os.path.sep.join((_mydir, 'requirements.txt')), "r").read().split('\n') if len(r) > 1]
 setup(
     name='ops-cli',
-    version='2.1.0',
+    version='2.1.1',
     description='Ops - wrapper for Terraform, Ansible, and SSH for cloud automation',
     long_description=_readme + '\n\n',
     long_description_content_type='text/markdown',
